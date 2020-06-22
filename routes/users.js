51c9b1a24b55c9885641c092237536fa29cf1d09
JM REMEUR <jm@remeur.fr>
2020-06-22 12:33:59 +0200

    express validator to validate request body handle errors

--- routes/users.js
+++ routes/users.js
@@ -1,27 +1,43 @@
 var express = require('express');
 var router = express.Router();
 var sequelize = require('../db.js');
+const { body, validationResult } = require('express-validator');
 
 /* GET users listing. */
-router.get('/', function(req, res, next) {
-  res.send('users\' respond');
+router.get('/', async (req, res, next) => {
+	var users = await sequelize.users.findAll();
+	res.json(users.map((user) => user.toJSON()))
 });
 
-router.post('/', async function(req, res, next) {
+router.post('/', [
+	body('username').not().isEmpty(),
+	body('password').matches('^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)[a-zA-Z\d]{8,}$'),
+	body('email').isEmail()
+],
+async (req, res, next) => {
 
-	console.log(req.body);
+	const errors = validationResult(req);
+	if (!errors.isEmpty()) {
+		return res.status(400).json({
+			errors: errors.array() 
+		});
+	}
+
 	try{
 		var response = await sequelize.users.create({
 			username: req.body.username,
 			password: req.body.password,
 			email: req.body.email
 		});
+
+		res.json(response.dataValues);
 	}
 	catch(e){
 		console.log(e);
+		res.status(400).json({
+			error: 'This username is already used'
+		});
 	}
-
-	res.json();
 });
 
 module.exports = router;
