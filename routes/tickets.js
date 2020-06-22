99b190e0d4979af84f3a7527a946a9487f50b312
Thomas LACAZE <contact@thomaslacaze.fr>
2020-06-22 10:14:49 +0200

    update comments

--- routes/tickets.js
+++ routes/tickets.js
@@ -1,7 +1,7 @@
 var express = require('express');
 var router = express.Router();
 
-/* GET users listing. */
+/* GET tickets listing. */
 router.get('/', function(req, res, next) {
   res.send('tickets\' respond');
 });
