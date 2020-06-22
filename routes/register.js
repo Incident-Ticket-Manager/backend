59c1834a8f4bbd48efff2fb9765715e2f93766f2
JM REMEUR <jm@remeur.fr>
2020-06-22 20:37:16 +0200

    swagger doc

--- routes/register.js
+++ routes/register.js
@@ -3,6 +3,28 @@ let router = express.Router();
 let sequelize = require('../db');
 const { body, validationResult } = require('express-validator');
 
+/**
+ * @typedef RegisterDTO
+ * @property {string} username - Username of the user
+ * @property {string} password - Password of the user
+ * @property {string} email - Email of the user
+ */
+
+/**
+ * @typedef Error
+ * @property {string} error - Register error
+ */
+
+/**
+ * Register a new user
+ * @route POST /register
+ * @group Authentification
+ * @param {RegisterDTO.model} register.body.required - Register body
+ * @consumes application/json
+ * @produces application/json
+ * @returns 200 - User registered
+ * @returns {Error.model} 400 - Username is already used
+ */
 router.post('/', [
 	body('username').not().isEmpty(),
 	body('password').matches(/^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)[a-zA-Z\d]{8,}$/),
@@ -24,7 +46,7 @@ async (req, res, next) => {
 			email: req.body.email
 		});
 
-		res.status(200).end();
+		res.json();
 	}
 	catch(e){
 		res.status(400).json({
