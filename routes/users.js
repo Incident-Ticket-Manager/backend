07a39bbd05a1ab00eabcbd041724a8e976979c6e
JM REMEUR <jm@remeur.fr>
2020-06-22 11:42:03 +0200

    post user

--- routes/users.js
+++ routes/users.js
@@ -1,9 +1,22 @@
 var express = require('express');
 var router = express.Router();
+var sequelize = require('../db.js');
 
 /* GET users listing. */
 router.get('/', function(req, res, next) {
   res.send('users\' respond');
+});
+
+router.post('/', async function(req, res, next) {
+
+	console.log(req);
+	var response = await sequelize.users.create({
+		username: req.body.username,
+		password: req.body.password,
+		email: req.body.email
+	})
+
+	res.json(response.dataValues);
 });
 
 module.exports = router;
