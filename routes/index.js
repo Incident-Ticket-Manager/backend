acec55e14b6226e36d230aa1b2dcbebb8e5585f7
JM REMEUR <jm@remeur.fr>
2020-06-22 15:13:01 +0200

    redirect index to swagger doc

--- routes/index.js
+++ routes/index.js
@@ -1,17 +1,9 @@
 let express = require('express');
 let router = express.Router();
 
-/**
- * This function comment is parsed by doctrine
- * @route GET /api
- * @group foo - Operations about user
- * @param {string} email.query.required - username or email - eg: user@domain
- * @param {string} password.query.required - user's password.
- * @returns {object} 200 - An array of user info
- * @returns {Error}  default - Unexpected error
- */
+/* GET tickets listing. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+	res.redirect('/docs');
 });
 
 module.exports = router;
