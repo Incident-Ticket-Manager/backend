ce06eade77f9f6fefdedfffebf2d6a18dcc56cdf
JM REMEUR <jm@remeur.fr>
2020-06-22 11:16:17 +0200

    update models with associations and uuid

--- models/project.js
+++ models/project.js
@@ -2,12 +2,17 @@
 
 module.exports = (sequelize, DataTypes) => {
     const Project = sequelize.define('projects', {
+		id: {
+			primaryKey: true,
+			type: DataTypes.UUID
+		},
 		name: DataTypes.STRING,
     }, {
         tableName: 'projects'
     });
     Project.associate = models => {
-        // associations can be defined here
+		// associations can be defined here
+		models.projects.hasMany(models.tickets);
 	};
 	Project.sync({force: true});
     return Project;
