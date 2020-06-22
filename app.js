685efd6b3c70f717ab850d5d7ec4ef0146368808
JM REMEUR <jm@remeur.fr>
2020-06-22 15:32:43 +0200

    update function create post to add projects

--- app.js
+++ app.js
@@ -71,7 +71,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
-app.use(express.json())
+app.use(express.json());
 app.use(expressJwt({
 	secret: process.env.SECRET
 }).unless({
@@ -89,20 +89,17 @@ app.use('/projects', projectsRouter);
 app.use('/tickets', ticketsRouter);
 
 // catch 404 and forward to error handler
-app.use(function(req, res, next) {
-  next(createError(404));
+app.use((req, res, next) => {
+	next(createError(404));
 });
 
 // error handler
-app.use(function(err, req, res, next) {
-  // set locals, only providing error in development
+app.use((err, req, res, next) => {
+	res.locals.message = err.message;
+	res.locals.error = req.app.get('env') === 'development' ? err : {};
 
-  res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
-
-  // render the error page
-  res.status(err.status || 500);
-  res.render('error');
+	res.status(err.status || 500);
+	res.render('error');
 });
 
 module.exports = app;
--- routes/projects.js
+++ routes/projects.js
@@ -1,9 +1,35 @@
 let express = require('express');
 let router = express.Router();
+let sequelize = require('../db');
+const { body, validationResult } = require('express-validator');
 
-/* GET projets listing. */
-router.get('/', function(req, res, next) {
-  res.send('projects\' respond');
+router.post('/', [
+	body('name').not().isEmpty()
+],
+async (req, res, next) => {
+
+	const errors = validationResult(req);
+	if (!errors.isEmpty()) {
+		return res.status(400).json({
+			errors: errors.array() 
+		});
+	}
+
+	try{
+		let response = await sequelize.projects.create({
+			name: req.body.name,
+			user: req.body.username
+		});
+
+		res.json(response.toJSON());
+	}
+	catch(e){
+		res.status(400).json({
+			error: 'This project name is already used'
+		});
+	}
+
+	res.status(200).end();
 });
 
 module.exports = router;
