25bc7fab2d553bb7ea4a8835acd79fe0bbb83999
JM REMEUR <jm@remeur.fr>
2020-06-22 18:50:00 +0200

    fix table name

--- models/user_project.js
+++ models/user_project.js
@@ -3,7 +3,7 @@
 const { Sequelize } = require('sequelize');
 
 module.exports = (sequelize, DataTypes) => {
-    const UserProject = sequelize.define('userProjects', {
+    const UserProject = sequelize.define('userProjects', {}, {
         tableName: 'user_projects'
     });
     return UserProject;
