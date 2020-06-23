c4d497a8bfc9c509af236c82c5e24b4d30ecb991
JM REMEUR <jm@remeur.fr>
2020-06-23 10:11:50 +0200

    project has many ticket

--- models/project.js
+++ models/project.js
@@ -13,6 +13,7 @@ module.exports = (sequelize, DataTypes) => {
     });
     Project.associate = models => {
 		models.project.belongsToMany(models.user, { through: models.userProjects });
+		models.project.hasMany(models.ticket);
 	};
     return Project;
 };
