cd6058ae0a2baf50e6e5ef84cc4d5cf8eadeb8ad
JM REMEUR <jm@remeur.fr>
2020-06-30 10:27:42 +0200

    delete user if admin

---
+++ routes/users.js
@@ -0,0 +1,40 @@
+let express = require('express');
+let router = express.Router();
+let sequelize = require('../db');
+const { body, validationResult } = require('express-validator');
+
+/**
+ * Delete user
+ * @route DELETE /users
+ * @group Users
+ * @consumes application/json
+ * @produces application/json
+ * @returns 200 - User deleted
+ * @returns {Error.model} 400 - User doesn't exists
+ * @returns 401 - User not authentified
+ * @security JWT
+ */
+router.delete('/:user', async (req, res, next) =>{
+	if(req.user.admin) {
+		let user = await sequelize.user.findOne({
+			where: {
+				id: req.params.user
+			}
+		});
+
+		if(user != null) {
+			await user.destroy();
+			res.json();
+		}
+		else {
+			res.json({
+				error: 'This user doesn\'t exists'
+			});
+		}
+	}
+	else {
+		res.json({
+			error: 'You are not an admin'
+		});
+	}
+});
