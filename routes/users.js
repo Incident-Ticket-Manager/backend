ba07d7def5dbcb1067e16120d73f35e1ffe963ed
JM REMEUR <jm@remeur.fr>
2020-06-30 11:27:57 +0200

    username and not name

--- routes/users.js
+++ routes/users.js
@@ -18,7 +18,7 @@ router.delete('/:user', async (req, res, next) =>{
 	if(req.user.admin) {
 		let user = await sequelize.user.findOne({
 			where: {
-				name: req.params.user
+				username: req.params.user
 			}
 		});
 
