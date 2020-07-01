4357950b29bc2161b6df309eae624b671a4c4967
JM REMEUR <jm@remeur.fr>
2020-07-01 21:00:34 +0200

    creating validators

--- validators.js
+++ validators.js
@@ -5,29 +5,24 @@ const validate = (req, res, next) => {
 	if (errors.isEmpty()) {
 		return next();
 	}
-	const extractedErrors = [];
-	errors.array().map(err => extractedErrors.push({ [err.param]: err.msg }));
+	const extractedErrors = []
+	errors.array().map(err => extractedErrors.push({ [err.param]: err.msg }))
 
 	return res.status(422).json({
 		errors: extractedErrors,
 	});
 };
 
-const registerValidation = () => {
-	return [
-		body('username').not().isEmpty().withMessage('Username required'),
-		body('password').matches(/^(?=.*?[A-Z])(?=.*[a-z])(?=.*[\d])(?!.*\s).{8,}$/)
-			.withMessage('Valid password required'),
-		body('email').isEmail().withMessage('Valid email required')
-	]
-};
+const registerValidation = [
+	body('username', 'Username required').not().isEmpty(),
+	body('password', 'Valid password required').matches(/^(?=.*?[A-Z])(?=.*[a-z])(?=.*[\d])(?!.*\s).{8,}$/),
+	body('email', 'Valid email required').isEmail()
+];
 
-const loginValidation = () => {
-	return [
-		body('username').not().isEmpty().withMessage('Username required'),
-		body('password').not().isEmpty().withMessage('Password required'),
-	]
-};
+const loginValidation = [
+	body('username', 'Username required').not().isEmpty(),
+	body('password', 'Password required').not().isEmpty()
+];
 
 module.exports = {
 	registerValidation,
