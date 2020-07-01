4ec1d2f7b0035c8c3c53e34235ff5e7ca57e30bd
JM REMEUR <jm@remeur.fr>
2020-07-01 21:00:17 +0200

    using bodyparser

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ let createError = require('http-errors');
 let express = require('express');
 let path = require('path');
 let cookieParser = require('cookie-parser');
+let bodyParser = require('body-parser');
 let logger = require('morgan');
 let cors = require('cors');
 
@@ -70,12 +71,12 @@ sequelize
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'pug');
 
-app.use(cors());
 app.use(logger('dev'));
+app.use(cors());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
-app.use(express.json());
+app.use(bodyParser.json());
 app.use(expressJwt({
 	secret: process.env.SECRET,
 	algorithms: ['RS256']
